1f99dad9a0011c57133cf66b3aa9df42cf343206
Carlos Rivera <carlosraulrp@gmail.com>
2023-07-08 22:05:05 -0400

    22:05

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import {ItemListContainer, ItemsDetailContainer} from './pages';
 import { Header } from './components/layouts/header/Header';
-import { useState } from 'react';
-import { CartContext } from './context/cartContext';
+import {CartProvider } from './context/cartContext';
 import Carrito from './components/cart';
 
 
@@ -20,40 +19,12 @@ import Carrito from './components/cart';
 
 
 function App() {
-  const [carrito, setCarrito] = useState([])
 
-  const agregarCarrito = (item, cantidad) =>{
-    const itemAgregado = {...item, cantidad}
-    const nuevoCarrito = [...carrito]
-    const estaEnCarrito = nuevoCarrito.find((producto)=> producto.id === itemAgregado.id)
-    if (estaEnCarrito){
-        estaEnCarrito.cantidad += cantidad
-        setCarrito(nuevoCarrito)
-    }else{
-        nuevoCarrito.push(itemAgregado)
-        
-    }
-    setCarrito(nuevoCarrito)
-   
-}
-
-const cantidadEnCarrito = () =>{
-  return carrito.reduce((acc, produc) => acc + produc.cantidad, 0)
-}
-
-const precioTotal = () =>{
-  return carrito.reduce((acc, produc) => acc + produc.cantidad * produc.price, 0)
-}
-
-const vaciarCarrito = () =>{
-  setCarrito([])
-}
- 
 
   return (
 
     <div>
-      <CartContext.Provider value={{carrito, agregarCarrito, cantidadEnCarrito, precioTotal, vaciarCarrito}}>
+      <CartProvider>
         <BrowserRouter>
           <Header/>
           <Routes>
@@ -68,7 +39,9 @@ const vaciarCarrito = () =>{
 
                 
         </BrowserRouter>
-      </CartContext.Provider>
+
+      </CartProvider>
+      
 
       </div>
     
--- src/context/cartContext/index.jsx
+++ src/context/cartContext/index.jsx
@@ -2,7 +2,43 @@ import { createContext, useState } from "react";
 
 const CartContext = createContext()
 
+const CartProvider = ({children}) =>{
+    const [carrito, setCarrito] = useState([])
+
+    const agregarCarrito = (item, cantidad) =>{
+      const itemAgregado = {...item, cantidad}
+      const nuevoCarrito = [...carrito]
+      const estaEnCarrito = nuevoCarrito.find((producto)=> producto.id === itemAgregado.id)
+      if (estaEnCarrito){
+          estaEnCarrito.cantidad += cantidad
+          setCarrito(nuevoCarrito)
+      }else{
+          nuevoCarrito.push(itemAgregado)
+          
+      }
+      setCarrito(nuevoCarrito)
+     
+  }
+  
+  const cantidadEnCarrito = () =>{
+    return carrito.reduce((acc, produc) => acc + produc.cantidad, 0)
+  }
+  
+  const precioTotal = () =>{
+    return carrito.reduce((acc, produc) => acc + produc.cantidad * produc.price, 0)
+  }
+  
+  const vaciarCarrito = () =>{
+    setCarrito([])
+  }
+
+  return (
+  
+    <CartContext.Provider value={{carrito, agregarCarrito, cantidadEnCarrito, precioTotal, vaciarCarrito}}>
+        {children}
+    </CartContext.Provider>
+  )
+}
 
 
-
-export {CartContext}
+export {CartContext, CartProvider}
